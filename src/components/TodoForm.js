18c0e7a12d3af07ce90ed37e238d24d573c4d8f8
Sean Birmingham <seannbirmingham98@gmail.com>
2020-08-31 16:53:10 -0400

    fixed clear button

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -29,9 +29,12 @@ export class TodoForm extends React.Component {
             placeholder='Enter a todo'
             value={this.state.todo}
             onChange={this.handleChange}
+            required
           />
           <button type='submit'>Add Todo</button>
-          <button onClick={this.props.clearCompleted}>Clear completed</button>
+          <button type='reset' onClick={this.props.clearCompleted}>
+            Clear completed
+          </button>
         </form>
       </div>
     );
